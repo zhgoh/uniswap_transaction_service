429c283d81474f476cb5b883c28bec30f2674fec
Goh Zi He <zihe.goh@gmail.com>
2022-04-23 09:55:40 +0800

    Added TODO

--- web/main.go
+++ web/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	// TODO: Have a goroutine running in the background to poll live data
+	// TODO: Provide REST api for the following,
+	// TODO: API: batch job
+	// TODO: API: Get transaction fee given transaction hash
+	// TODO: Bonus API: Decode actual Uniswap swap price
 	// Handle route using handler func
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to new server!")
