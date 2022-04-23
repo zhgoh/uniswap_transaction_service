7860cae8ac02efb28e1ad2ac6de9bd52f363c9cf
Goh Zi He <zihe.goh@gmail.com>
2022-04-23 10:45:59 +0800

    Added more structures

--- web/main.go
+++ web/main.go
@@ -6,18 +6,36 @@ import (
 )
 
 func main() {
+	fmt.Print("Transactions service")
 	// TODO: Have a goroutine running in the background to poll live data
+
+	// Serve
+	serve("5050")
+}
+
+func serve(port string) {
 	// TODO: Provide REST api for the following,
-	// TODO: API: batch job
-	// TODO: API: Get transaction fee given transaction hash
-	// TODO: Bonus API: Decode actual Uniswap swap price
 	// Handle route using handler func
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to new server!")
+		fmt.Fprintf(w, "Endpoints \n")
+		fmt.Fprintf(w, "POST /batch")
+		fmt.Fprintf(w, "GET  /transactions/<hash>")
 	})
 
+	// TODO: API: batch job
+	http.HandleFunc("/batch", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Batch")
+	})
+
+	// TODO: API: Get transaction fee given transaction hash
+	http.HandleFunc("/transactions", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Transactions")
+	})
+
+	// TODO: Bonus API: Decode actual Uniswap swap price
+
 	// Listen to port
-	const port = ":5050"
 	fmt.Printf("Running server at port at %s", port)
+	port = fmt.Sprintf(":%s", port)
 	http.ListenAndServe(port, nil)
 }
